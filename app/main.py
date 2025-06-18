8fc106ad3bcba2371410209df5b4af611a76a654
Divyanshu Gupta <divyanshugupta068@gmail.com>
2025-06-18 14:57:30 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,8 +1,22 @@
 # app/main.py
 
 from fastapi import FastAPI
+from app.api import routes 
+from app.api import auth
 from fastapi.middleware.cors import CORSMiddleware
+from dotenv import load_dotenv
 from app.api.routes import api_router
+from app.models.database import Base, engine  # Import for DB creation
+import asyncio
+import os 
+
+app= FastAPI()
+
+app.include_router(auth.router)  # Include auth routes
+app.include_router(routes.api_router)  # Include other API routes
+
+load_dotenv()  # Load environment variables from .env file
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 
 app = FastAPI(
     title="MediScan AI",
@@ -23,3 +37,9 @@ app.include_router(api_router, prefix="/api")
 @app.get("/")
 def read_root():
     return {"message": "Welcome to MediScan AI API"}
+
+# 👇 Create database tables on startup
+@app.on_event("startup")
+async def startup_event():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
